7722704e08b2aa9779124685d366b2ad8586eed8
Josh Quintal <josh@trufflesuite.com>
2019-09-06 00:59:40 -0400

    Built contracts

--- truffle-config.js
+++ truffle-config.js
@@ -1,21 +1,17 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
 module.exports = {
-  // Uncommenting the defaults below 
-  // provides for an easier quick-start with Ganache.
-  // You can also follow this format for other networks;
-  // see <http://truffleframework.com/docs/advanced/configuration>
-  // for more details on how to specify configuration options!
-  /*
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*"
+      host: '127.0.0.1',
+      port: 9545,
+      network_id: '*'
     },
-    test: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*"
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider("4c226bfa6fb7e66f4f8219fda65a708622e3bfc8ae3f5052f8983ba3c1e6574b", "https://ropsten.infura.io/v3/416c36ceb9ca4f2aac6f3603464ca5f0");
+      },
+      network_id: '3',
     }
   }
-  */
 };
